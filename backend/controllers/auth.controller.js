e3e62cae669df35a166fad72c05cf55cba4236fc
AravaTanish <aravatanish@gmail.com>
2026-04-01 19:53:46 +0530

    Minor changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -104,8 +104,8 @@ export const verify = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
